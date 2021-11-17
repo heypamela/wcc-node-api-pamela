b90deff59448d69e8f7eeb1332196d0237336dbc
Pamela <pamsilva.trabalho@hotmail.com>
2021-11-16 21:54:24 -0300

    Base do sistema de artigos

--- index.js
+++ index.js
@@ -5,43 +5,12 @@ const app = express();
 const port = 8080;
 
 
-//GET - obter algo
-//POST- criar algo
-//PUT - atualizar algo
-//DELETE - deletar algo
-
 app.use(express.json());
 
 app.get("/", function(req, res){
-    res.send("Minha primeira requisão");
+    res.send("Dasa Educa - Artigos");
 });
 
-app.get("/segunda-req", function(req, res){
-    res.send("Minha segunda requisição!" );
-});
-
-app.get("/com-parametros", function(req, res){
-    if(req.query.nome === "Pâmela"){
-        res.send("Pâmela chamou requisição!");
-    }
-    res.send("Com parâmetros funciona!Sabadou "+ req.query.nome);
-});
-
-app.post("/meu-primeiro-post", function(req,res){
-    console.log(req.body);
-    res.send("Meu post funciona");
-});
-
-app.put("/meu-primeiro-put/:id", function(req, res){
-    console.log(req.body, req.params.id);
-    res.send("Meu PUT funciona");
-});
-
-
-app.delete("/meu-primeiro-delete/:id", function(req, res){
-    console.log(req.params.id);
-    res.send("Meu delete funciona " + req.params.id);
-})
 
 app.listen(port, function(){
     console.log("Ouvindo a porta: ", port);
---
+++ routes/artigos.routes.js
@@ -0,0 +1,20 @@
+// Rotas do sistemas de artigos
+
+/*
+    GET
+    -Obter todos os artigos
+    -Obeter um artigo especifico
+    -Obter todos os artigos publicados
+
+
+    POS
+    -Criar um novo artigo
+
+    PUT
+    -Publicar meu artigo
+
+    DELETE
+    -Deletar um artigo
+
+*/
+
