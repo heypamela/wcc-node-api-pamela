2621b9c5ca4d7b93f998e5e92bb8e1edc6dd785f
Pamela <pamsilva.trabalho@hotmail.com>
2021-11-16 21:39:36 -0300

    Minhas primeiras rotas

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 //PARA RODAR O SERVIDOR npm run start
+//npm install --save-dev nodemon
 const express = require("express");
 const app = express();
 const port = 8080;
+
+
+//GET - obter algo
+//POST- criar algo
+//PUT - atualizar algo
+//DELETE - deletar algo
+
+app.use(express.json());
 
 app.get("/", function(req, res){
     res.send("Minha primeira requisão");
@@ -12,7 +21,26 @@ app.get("/segunda-req", function(req, res){
 });
 
 app.get("/com-parametros", function(req, res){
+    if(req.query.nome === "Pâmela"){
+        res.send("Pâmela chamou requisição!");
+    }
     res.send("Com parâmetros funciona!Sabadou "+ req.query.nome);
+});
+
+app.post("/meu-primeiro-post", function(req,res){
+    console.log(req.body);
+    res.send("Meu post funciona");
+});
+
+app.put("/meu-primeiro-put/:id", function(req, res){
+    console.log(req.body, req.params.id);
+    res.send("Meu PUT funciona");
+});
+
+
+app.delete("/meu-primeiro-delete/:id", function(req, res){
+    console.log(req.params.id);
+    res.send("Meu delete funciona " + req.params.id);
 })
 
 app.listen(port, function(){
